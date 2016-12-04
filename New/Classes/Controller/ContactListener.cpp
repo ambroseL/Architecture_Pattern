f35de1a519899d028fb2c047217f13962bd5c94d
yzc19941020 <304614345@qq.com>
2016-12-04 22:36:06 +0800

    把thisLayer删掉了
    iiiiiiiiiiiiii

--- New/Classes/Controller/ContactListener.cpp
+++ New/Classes/Controller/ContactListener.cpp
@@ -1,5 +1,4 @@
 #include "ContactListener.h"
-#include "cocos2d.h"
 
 #define BALL_TO_WALL 1
 #define BALL_TO_BRICK 2
@@ -7,9 +6,33 @@
 #define PADDLE_TO_WALL 4
 #define PADDLE_TO_BALL 5
 
+EventHandler* ContactListener::createStrategy(int EventType, b2Body* bodyA, b2Body* bodyB, b2Contact* _contact, ObjManager* objManager)
+{
+	EventHandler *newStrategy = NULL;
+	switch (EventType)
+	{
+	case 1:
+		newStrategy = new BallToWall(bodyA, bodyB, _contact, objManager);
+		break;
+	case 2:
+		newStrategy = new BallToBrick(bodyA, bodyB, _contact, objManager);
+		break;
+	case 3:
+		newStrategy = new PaddleToPack(bodyA, bodyB, _contact, objManager);
+		break;
+	case 4:
+		newStrategy = new PaddleToWall(bodyA, bodyB, _contact, objManager);
+		break;
+	case 5:
+		newStrategy = new PaddleToBall(bodyA, bodyB, _contact, objManager);
+		break;
+	}
+	return newStrategy;
+}
 
 void ContactListener::BeginContact(b2Contact* contact)
 {
+	isBrick = false;
 	//碰撞开始时的回调方法
 	b2Body* bodyA = contact->GetFixtureA()->GetBody();
 	b2Body* bodyB = contact->GetFixtureB()->GetBody();
@@ -17,26 +40,29 @@ void ContactListener::BeginContact(b2Contact* contact)
 	{
 		std::string* aid = (std::string*)bodyA->GetUserData();
 		std::string* bid = (std::string*)bodyB->GetUserData();
-
 		char preFixA = aid->at(0);
 		char preFixB = bid->at(0);
 		EventHandler *newEvent = NULL;
-		if (ball->isPermeat())
-			ballVec = ball->getBody()->GetLinearVelocity();
+		bool isPermeat = objManager->getBallPermeat();
+		if (isPermeat)
+			ballVec = objManager->getBallSpeed();
 		if ((preFixA == 'Q' || preFixB == 'Q') && (preFixA == 'W' || preFixB == 'W'))//球与墙的碰撞
-			newEvent = Strategy(BALL_TO_WALL, bodyA, bodyB, contact, thisLayer, ball, paddle);
+			newEvent = createStrategy(BALL_TO_WALL, bodyA, bodyB, contact, objManager);
 
 		if ((preFixA == 'Q' || preFixB == 'Q') && (preFixA == 'B' || preFixB == 'B'))//球与砖块的碰撞
-			newEvent = Strategy(BALL_TO_BRICK, bodyA, bodyB, contact, thisLayer, ball, paddle);
+		{
+			isBrick = true;
+			newEvent = createStrategy(BALL_TO_BRICK, bodyA, bodyB, contact, objManager);
+		}
 
 		if ((preFixA == 'P' || preFixB == 'P') && (preFixA == 'A' || preFixB == 'A'))//挡板与包裹的碰撞
-			newEvent = Strategy(PADDLE_TO_PACK, bodyA, bodyB, contact, thisLayer, ball, paddle);
+			newEvent = createStrategy(PADDLE_TO_PACK, bodyA, bodyB, contact, objManager);
 
 		if ((preFixA == 'P' || preFixB == 'P') && (preFixA == 'W' || preFixB == 'W'))//挡板与墙的碰撞
-			newEvent = Strategy(PADDLE_TO_WALL, bodyA, bodyB, contact, thisLayer, ball, paddle);
+			newEvent = createStrategy(PADDLE_TO_WALL, bodyA, bodyB, contact, objManager);
 
 		if ((preFixA == 'P' || preFixB == 'P') && (preFixA == 'Q' || preFixB == 'Q'))//挡板与球的碰撞
-			newEvent = Strategy(PADDLE_TO_BALL, bodyA, bodyB, contact, thisLayer, ball, paddle);
+			newEvent = createStrategy(PADDLE_TO_BALL, bodyA, bodyB, contact, objManager);
 
 		newEvent->doStrategy();
 	}
@@ -44,9 +70,9 @@ void ContactListener::BeginContact(b2Contact* contact)
 
 void ContactListener::EndContact(b2Contact* contact)
 {
-	if (ball->isPermeat())
+	if (objManager->getBallPermeat()&&isBrick)
 	{
-		ball->getBody()->SetLinearVelocity(ballVec);
+		objManager->setBallSpeed(ballVec);
 	}
 }
 
--- New/Classes/Controller/EventHandler.h
+++ New/Classes/Controller/EventHandler.h
@@ -1,58 +1,37 @@
 #ifndef  _EventHandler_H_
 #define  _EventHandler_H_
 #include "cocos2d.h"
-#include "Beginlayer.h"
-#include <Box2D/Box2D.h>
-
-#define PACK 1
-#define BRICK 2
-#define SOUND 3
+#include "Box2D/Box2D.h""
+#include "GameLayer.h"
+#include "ObjManager.h"
 
 using namespace cocos2d;
+using namespace std;
 
-struct eventObj
-{
-	eventObj(int eventtype, int hp, std::string id)
-	{
-		eventType = eventtype;
-		HP = hp;
-		Id = id;
-	}
-	int eventType;
-	int HP;
-	std::string Id;
-};
+
 class EventHandler
 {
 public:
 	virtual void doStrategy() {};
-	EventHandler(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-	{
-		bodyA = body_A;
-		bodyB = body_B;
-		contact = _contact;
-		thisLayer = _thisLayer;
-		ball = _ball;
-		paddle = _paddle;
-	}
-	static std::vector<eventObj*> eventQueue;
+	EventHandler(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager):
+	bodyA(body_A),bodyB(body_B),contact(_contact),thisLayer(_thisLayer),objManager(objManager){}
+	
+
 protected:
-	BallObj* ball;
-	PaddleObj* paddle;
 	b2Body* bodyA;
 	b2Body* bodyB;
 	b2Contact* contact;
-	Beginlayer*  thisLayer;
-
-
+	ObjManager* objManager;
 };
+
+
 
 class BallToBrick : public EventHandler  //球与砖块的策略类
 {
 public:
 	virtual void doStrategy();
-	BallToBrick(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-		: EventHandler(body_A, body_B, _contact, _thisLayer, _ball, _paddle) {};
+	BallToBrick(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager)
+		: EventHandler(body_A, body_B, _contact, objManager) {};
 
 };
 
@@ -60,8 +39,8 @@ class BallToWall : public EventHandler  //球与墙的策略类
 {
 public:
 	virtual void doStrategy();
-	BallToWall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-		: EventHandler(body_A, body_B, _contact, _thisLayer, _ball, _paddle) {};
+	BallToWall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager)
+		: EventHandler(body_A, body_B, _contact, objManager) {};
 
 };
 
@@ -69,8 +48,8 @@ class PaddleToPack : public EventHandler  //挡板与包裹的策略类
 {
 public:
 	virtual void doStrategy();
-	PaddleToPack(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-		: EventHandler(body_A, body_B, _contact, _thisLayer, _ball, _paddle) {};
+	PaddleToPack(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager)
+		: EventHandler(body_A, body_B, _contact, objManager) {};
 
 };
 
@@ -78,8 +57,8 @@ class PaddleToWall : public EventHandler  //挡板与墙的策略类
 {
 public:
 	virtual void doStrategy();
-	PaddleToWall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-		: EventHandler(body_A, body_B, _contact, _thisLayer, _ball, _paddle) {};
+	PaddleToWall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager)
+		: EventHandler(body_A, body_B, _contact, objManager) {};
 
 };
 
@@ -87,8 +66,8 @@ class PaddleToBall : public EventHandler  //挡板与球的策略类
 {
 public:
 	virtual void doStrategy();
-	PaddleToBall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, Beginlayer* _thisLayer, BallObj* _ball, PaddleObj* _paddle)
-		: EventHandler(body_A, body_B, _contact, _thisLayer, _ball, _paddle) {};
+	PaddleToBall(b2Body* body_A, b2Body* body_B, b2Contact* _contact, ObjManager* objManager)
+		: EventHandler(body_A, body_B, _contact, objManager) {};
 
 };
 #endif
